6d6829ccc0a33624136c9e9e53b52f3500f89b8a
Duy <duyvu372001@gmail.com>
2024-12-09 13:04:12 +0900

    deploy

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,13 +1,31 @@
 require('dotenv').config()
 
 const { Sequelize } = require('sequelize')
-const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT } = process.env
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT, DB_PORT } = process.env
+const {
+  MYSQL_ADDON_HOST,
+  MYSQL_ADDON_DB,
+  MYSQL_ADDON_USER,
+  MYSQL_ADDON_PORT,
+  MYSQL_ADDON_PASSWORD,
+} = process.env
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: 'localhost',
-  dialect: DB_DIALECT,
-  logging: false,
-})
+const sequelize = new Sequelize(
+  MYSQL_ADDON_DB,
+  MYSQL_ADDON_USER,
+  MYSQL_ADDON_PASSWORD,
+  {
+    host: MYSQL_ADDON_HOST,
+    dialect: 'mysql',
+    port: MYSQL_ADDON_PORT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+)
 let connectDB = async () => {
   try {
     await sequelize.authenticate()
