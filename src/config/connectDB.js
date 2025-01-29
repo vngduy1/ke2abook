7a2866e7f34ab48f18a142178e6bf32f33f0a8d3
Duy <duyvu372001@gmail.com>
2025-01-29 09:28:57 +0900

    kaizen

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT, DB_PORT } = process.env
 
-//DEPLOY用
+//deploy用
 const {
   MYSQL_ADDON_HOST,
   MYSQL_ADDON_DB,
@@ -13,29 +13,29 @@ const {
 } = process.env
 
 //local用
-// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-//   host: DB_HOST,
-//   dialect: DB_DIALECT,
-//   logging: false,
-// })
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  logging: false,
+})
 
 //DEPLOY用
-const sequelize = new Sequelize(
-  MYSQL_ADDON_DB,
-  MYSQL_ADDON_USER,
-  MYSQL_ADDON_PASSWORD,
-  {
-    host: MYSQL_ADDON_HOST,
-    dialect: 'mysql',
-    port: MYSQL_ADDON_PORT,
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  },
-)
+// const sequelize = new Sequelize(
+//   MYSQL_ADDON_DB,
+//   MYSQL_ADDON_USER,
+//   MYSQL_ADDON_PASSWORD,
+//   {
+//     host: MYSQL_ADDON_HOST,
+//     dialect: 'mysql',
+//     port: MYSQL_ADDON_PORT,
+//     logging: false,
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   },
+// )
 
 let connectDB = async () => {
   try {
